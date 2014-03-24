3a7083731184be6747d6486da2471c697d80d2e5
Cale <cale.t.b@gmail.com>
2014-03-24 10:58:14 -0400

    Final updates needed

--- StockMarketWatch/src/CONSTANTS.java
+++ StockMarketWatch/src/CONSTANTS.java
@@ -4,7 +4,7 @@ public class CONSTANTS {
 	public static int NUMBEROFSTOCKS = 20;
 	
 	public static String STRATEGY_PATTERN_INFO = 
-			"This is where we will post the information about the strategy patterns.\n This should pop up on a menu or dialog box.";
+			"<html>This is where we will post the information about the <b>strategy patterns</b>.\n This should pop up on a menu or dialog box.";
 	public static String ABOUT =
 			"Developed By:\n \t Ryan Mulligan\n \t Cale Bowen";
 }
